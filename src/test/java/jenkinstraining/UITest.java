a984c1d6da0822c696a8a7e504f4fb93fe5ae856
shashibhushanvirajaji <shashi.virajaji@gmail.com>
2021-09-02 15:10:23 +0530

    first commit

---
+++ src/test/java/jenkinstraining/UITest.java
@@ -0,0 +1,28 @@
+package jenkinstraining;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class UITest {
+
+	@Test
+	public void firstTest() {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.google.com/");
+		Assert.assertEquals(driver.getTitle(), "Google");
+		
+		driver.quit();
+		
+	}
+	
+}
