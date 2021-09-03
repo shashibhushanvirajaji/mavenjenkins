e5bbe0eafedb5056a420a31f75a5f77193da0e27
shashibhushanvirajaji <shashi.virajaji@gmail.com>
2021-09-03 07:33:33 +0530

    paramter added

--- src/test/java/jenkinstraining/UITest.java
+++ src/test/java/jenkinstraining/UITest.java
@@ -4,19 +4,31 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest {
 
+	@Parameters("Browser")
 	@Test
-	public void firstTest() {
+	public void firstTest(String browsername) {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = null;
 		
+		if(browsername.contains("Chrome"))
+		{
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+		else 
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("https://www.google.com/");
 		Assert.assertEquals(driver.getTitle(), "Google");
